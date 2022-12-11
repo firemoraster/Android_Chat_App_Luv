865a65ccfb5fb280ca92a920fd457dc19144c40c
firemoraster <xkishou@gmail.com>
2022-12-11 13:35:18 +0200

    12/11/2022 update: bindings set; lauch options updated ( R.layout to inflate)

--- app/src/main/java/com/plemiannyk/luv/activities/SignInActivity.java
+++ app/src/main/java/com/plemiannyk/luv/activities/SignInActivity.java
@@ -2,15 +2,31 @@ package com.plemiannyk.luv.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.plemiannyk.luv.R;
+import com.plemiannyk.luv.databinding.ActivitySignInBinding;
 
 public class SignInActivity extends AppCompatActivity {
+
+
+    private ActivitySignInBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        binding=ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
     }
+
+    private void setListeners()
+    {
+         binding.textCreateNewAccount.setOnClickListener(v ->
+
+                 startActivity(new Intent(getApplicationContext(),SignUpActivity.class)));
+    }
+
+
 }
